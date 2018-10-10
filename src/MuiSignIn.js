649d65c8f3cafd01f38ac60309496bdfd1b1db7c
changtimwu <changtimwu@gmail.com>
2018-10-11 02:28:42 +0800

    fix the syntax

--- src/MuiSignIn.js
+++ src/MuiSignIn.js
@@ -11,7 +11,7 @@ import InputLabel from '@material-ui/core/InputLabel'
 import Icon from '@material-ui/core/Icon'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import { withStyles } from '@material-ui/core/styles/withStyles'
+import withStyles from '@material-ui/core/styles/withStyles'
 
 const styles = theme => ({
   layout: {
