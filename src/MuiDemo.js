a4b88c863aaf3c5ba188072a17fd506a9a911ff9
changtimwu <changtimwu@gmail.com>
2018-10-11 02:29:08 +0800

    add list dividers

--- src/MuiDemo.js
+++ src/MuiDemo.js
@@ -1,12 +1,13 @@
 import * as React from 'react'
-import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button'
+import MuiListDividers from './MuiListDividers'
 const MuiDemo = () => (
     <div>
         <h2>Material UI Demo</h2>
         <Button variant="raised" color="primary">
-          Hello World
+            Hello World
         </Button>
+        <MuiListDividers />
     </div>
 )
 export default MuiDemo
